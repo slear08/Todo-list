7406ebd483c03003f9b6e39e06040d7e55a6f48d
slear08 <88441444+slear08@users.noreply.github.com>
2022-08-20 22:22:12 +0800

    Animation added and JavaScript function button

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const todoButton = document.querySelector('.todo-button');
 const todos = document.querySelector('.todos');
 
 todoButton.addEventListener('click', addTodoList);
-
+todos.addEventListener('click', completeAndDeleteTodos);
 function addTodoList(e){
     e.preventDefault()
 
@@ -28,4 +28,23 @@ function addTodoList(e){
     
 
     todos.appendChild(createElement);
+    todoInput.value = "";
+}
+
+function completeAndDeleteTodos(e){
+    const compAndDel = e.target;
+
+    if(compAndDel.classList.contains("trash-btn")){
+        const toDelete = compAndDel.parentElement;
+        toDelete.classList.add("remove-todo");
+        
+        toDelete.addEventListener("transitionend", (e)=>{
+            toDelete.remove();
+        })
+    }
+    if(compAndDel.classList.contains("complete-btn")){
+        const toComplete = compAndDel.parentElement;
+        toComplete.classList.toggle("completed-todo");
+    }
+
 }
